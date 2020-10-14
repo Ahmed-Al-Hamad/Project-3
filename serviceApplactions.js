20ad3b0f13c1c062c9f352eb4579c24fd5237703
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2020-10-14 16:25:16 +0300

    second commit

--- serviceApplactions.js
+++ serviceApplactions.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db={orgnization:{},
+          service:{},
+          user:{} } 
+  const register={}
+const informationUser={};
+app.get("/",(req,res)=>{
 
+})
+app.post("/",(req,res)=>{
+
+})
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
