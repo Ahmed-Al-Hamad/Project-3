fe9b0d9cbf349c4aa726a8a84c4be18ea408deb9
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2020-10-15 21:26:33 +0300

    third commit

--- serviceApplactions.js
+++ serviceApplactions.js
@@ -6,11 +6,18 @@ const db={orgnization:{},
           user:{} } 
   const register={}
 const informationUser={};
-app.get("/",(req,res)=>{
-
+app.use(express.json())
+app.get("/h/:typ",(req,res)=>{
+  if(req.params.typ==="add"){
+res.json("true") ; 
+console.log(req.params.typ)
+  }else res.json("false")
 })
-app.post("/",(req,res)=>{
 
+app.post('/',(req,res)=>{
+ 
+console.log(req.body.name)
+res.json(req.body.name)
 })
 
 app.listen(port, () => {
