47451c24b08eca330857a5c5c9345f41b466dad9
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2020-10-17 14:21:19 +0300

    eleventh commit

--- serviceApplactions.js
+++ serviceApplactions.js
@@ -9,34 +9,48 @@ const informationUser={};
 app.use(express.json())
 // registeration user
 const registerUser=(id_body,firstName_body,middleName_body,lasteName_body,phNum_body,address_body,email_body,password_body)=>{
+  let result
 if(id_body>0&&id_body!==0){
   informationUser.id=id_body
-}else return "pleas the id is importent you should insert it"
+}else result+= "pleas the id is importent you should insert it"+" "
 if(firstName_body!==" "){
   informationUser.firstName=firstName_body
-}else return " the first name is requrid"
+}else result+= " the first name is requrid"+" "
 if(middleName_body!==" "){
   informationUser.middleName=middleName_body
-}else return " the middle name is requrid"
+}else result+= " the middle name is requrid"+" "
 if(lasteName_body!==" "){
   informationUser.lastName=lasteName_body
-}else return " the last name is requrid"
+}else result+= " the last name is requrid"+" "
 if(phNum_body!==" "){
   informationUser.phNum=phNum_body
-}else return " the phon number is requrid"
+}else result+= " the phon number is requrid"+" "
 if(address_body!==" "){
   informationUser.address=address_body
-}else return " the address is requrid"
+}else result+= " the address is requrid"+" "
 if(email_body!==" "){
   informationUser.email=email_body
-}else return " the email is requrid"
+}else result+=" the email is requrid"+" "
 if(password_body!==" "){
   informationUser.passowrd=password_body
-}else return " the password is requrid"
+}else result+= " the password is requrid"+" "
+return result
 }
 app.post("/registerNewUser",(req,res)=>{
-  registerUser(req.body.id,req.body.firstName,req.body.middleName,req.body.lastName,req.body.phNum,req.body.address,req.body.email,req.body.passowrd)
+let result=  registerUser(req.body.id,req.body.firstName,req.body.middleName,req.body.lastName,req.body.phNum,req.body.address,req.body.email,req.body.passowrd)
+res.json(result)
 })
+// function login
+app.get("/login",(req,res)=>{
+  if(req.body.email===undefined&&req.body.passowrd===undefined){
+    res.json("Please enter your email and password")
+ 
+  }else if(req.body.email===informationUser.email&&req.body.passowrd===informationUser.passowrd){
+    res.json("login successfully")
+  }
+  else res.json("Please create an account on the site to be able to benefit from the services")
+  })
+  
 //function to Add Admin
 const addmin_Opration=(typOperation,data)=>{
   if(typOperation==="add-service") {
